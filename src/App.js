445ae1fdacbd38b81fa936a90461863e70291747
devsurimlee <devsurimlee@gmail.com>
2020-03-19 14:08:22 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Hello from './Hello';
-import './App.css';
+import React from "react";
+import Hello from "./Hello";
+import "./App.css";
 
 function App() {
-  const name = 'react!';
+  const name = "react!";
   const divStyle = {
-    backgroundColor: 'black',
-    color: 'white',
-    padding: '10px'
+    backgroundColor: "black",
+    color: "white",
+    padding: "10px"
   };
   return (
     <>
-    <hi
+      <hi
       // 주석 스타일 1번
-    />
-    {/* 주석 스타일 2번 */}
-    {/* 
+      />
+      {/* 주석 스타일 2번 */}
+      {/* 
       2개 이상 태그는 꼭 중갈호{}로 감싸거나 self-closing 하기,
-      js값 보여줄땐 중갈호로 처리
-      style은 문자형태X 객체형태로 넣음{divStyle}, class -> className 대체 
+      js값 보여줄땐 중갈호로 처리,
+      style은 문자형태X 객체형태로 넣음{divStyle}, class -> className 대체함; 
     
     */}
 
@@ -27,6 +27,7 @@ function App() {
       <Hello />
       <div style={divStyle}>{name}</div>
       <div className="gray-box"></div>
+      <div>test</div>
     </>
   );
 }
