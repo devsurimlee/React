884af0830477445c00d864d9f3d6d0ed71c37bcb
devsurimlee <devsurimlee@gmail.com>
2020-03-19 17:04:41 +0900

    커밋

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function Hello() {
-    return <div>안녕하세요 리엑트!!</div>
+function Hello({name, color}) {
+return <div style={{
+    background: '#000000' ,
+    color: color
+}} >안녕하세요! {name}</div>
 }
+
+Hello.defaultProps = {
+    name: '이름없음',
+    color: 'pink'
+};
 
 export default Hello;
